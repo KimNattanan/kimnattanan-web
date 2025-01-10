8c7a3402a86767a619615d801217d1d6f73e1392
KimNattanan <jeung.nattanan@gmail.com>
2025-01-10 21:37:33 +0700

    ??

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -7,18 +7,20 @@ const imgStyle = useTitleImgStyleStore();
 </script>
 <template>
 
-  <div class="h-[calc(100vh-6rem)] flex justify-end items-start pb-36">
-    <div class="font-serif h-full content-center mr-20">
+  <div class="h-[calc(100vh-6rem)] flex items-start pb-36">
+    <div class="font-serif h-full content-center ml-32">
       <div class="text-7xl py-2">Kim,</div>
       <div class="text-2xl py-2 ">．。．。．。．。．。．。．。．。．。．。．。．。．。．。．。．。．。．。．。．</div>
     </div>
-    <img
-      id="titleImg"
-      class="object-contain max-w-[60vw] max-h-full transition-opacity duration-500 select-none"
-      :style="{opacity: imgStyle.opac}"
-      src="/purikone.jpg"
-      draggable="false"
-    />
+    <div class="flex-1 flex justify-end">
+      <img
+        id="titleImg"
+        class="object-contain max-w-[60vw] max-h-full transition-opacity duration-500 select-none"
+        :style="{opacity: imgStyle.opac}"
+        src="/purikone.jpg"
+        draggable="false"
+      />
+    </div>
   </div>
   
 </template>
