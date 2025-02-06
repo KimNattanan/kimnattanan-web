12a06c975fbe8c93f56124bd3e16420d69af70ec
KimNattanan <jeung.nattanan@gmail.com>
2025-02-06 21:08:46 +0700

    java-proj

--- src/views/OfficeSurvivalView.vue
+++ src/views/OfficeSurvivalView.vue
@@ -4,23 +4,8 @@ import { onMounted, ref } from 'vue';
 import { goNewTab } from '@/assets/actions';
 import CoolDiv from '@/components/CoolDiv.vue';
 
-const codeBlock = ref<HTMLDivElement|null>(null);
-
 onMounted(()=>{
   scrollTo(0,0);
-  if(codeBlock.value){
-    codeBlock.value.animate(
-      [
-      {backgroundColor: 'rgb(255,255,255)'},
-      {backgroundColor: 'rgba(253, 193, 216,0.4)'},
-      {backgroundColor: 'rgb(255,255,255)'},
-      ],
-      {
-        duration: 1000,
-        iterations: Infinity,
-      }
-    );
-  }
 })
 
 </script>
@@ -40,27 +25,25 @@ onMounted(()=>{
                 sm:px-12
                 xs:px-8
                 px-6">
-      <div class="p-6 border-2 border-col3-2 w-fit" ref="codeBlock">
-        <div class="flex my-2">
-          Source code:
-          <a href="https://github.com/KimNattanan/java-project" target="_blank">
-            <CoolDiv class="text-col3-4 w-fit">
-              【Github】
-            </CoolDiv>
-          </a>
-        </div>
-        <div class="my-2">
-          Latest release:
-          <div class="ml-4">
-            <div class="flex">
-              <a href="https://github.com/KimNattanan/java-project/releases/download/v.0.1-alpha/OfficeSurvival.zip" target="_blank">
-                <CoolDiv class="text-col3-4 w-fit">
-                  【Download】
-                </CoolDiv>
-              </a>
-            </div>
-            Please run 「bin\app.bat」 or the shortcut file in the root folder.
+      <div class="flex my-2">
+        Source code:
+        <a href="https://github.com/KimNattanan/java-project" target="_blank">
+          <CoolDiv class="text-col3-4 w-fit">
+            【Github】
+          </CoolDiv>
+        </a>
+      </div>
+      <div class="my-2">
+        Release:
+        <div class="ml-4">
+          <div class="flex">
+            <a href="https://github.com/KimNattanan/java-project/releases/download/v.0.1/OfficeSurvival.zip" target="_blank">
+              <CoolDiv class="text-col3-4 w-fit">
+                【Download】
+              </CoolDiv>
+            </a>
           </div>
+          Please run 「bin\app.bat」 or the shortcut file in the root folder.
         </div>
       </div>
       <div class="my-8">
